ad763a39e776c66f17cec36491677d92cd3a5644
Monixc <chris13404@gmail.com>
2024-02-14 23:44:22 +0900

    색상 변경

--- color_change/content-script.js
+++ color_change/content-script.js
@@ -1,10 +1,9 @@
 document.addEventListener("keyup", function (e) {
   if (e.keyCode === 82) {
-    // 82는 R 키의 키 코드입니다.
     let comments = document.querySelectorAll("ytd-comment-renderer");
 
     comments.forEach((comment) => {
-      comment.style.background = "#acacac"; // 원하는 색상 코드로 변경해주시면 됩니다.
+      comment.style.background = "#111111";
     });
   }
 });
